75c4572df29b547c7f1111f3c27b0fedb9f852a4
Joshua Endert <joshua95@gmx.net>
2015-02-12 15:37:20 +0100

    adafdjskhjfahshhda

---
+++ Soulmate Remastered/Soulmate Remastered/Classes/GameStatesFolder/AbstractMainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soulmate_Remastered.Classes.GameStatesFolder
+{
+    abstract class AbstractMainMenu : GameState
+    {
+
+    }
+}
